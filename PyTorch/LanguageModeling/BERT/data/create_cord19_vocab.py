968173b92cf1fad6bc74d0d12246816432e2a73c
Greg Pauloski <jgpauloski@uchicago.edu>
2020-10-21 15:04:52 -0700

    FIX: uncomment code

--- PyTorch/LanguageModeling/BERT/data/create_cord19_vocab.py
+++ PyTorch/LanguageModeling/BERT/data/create_cord19_vocab.py
@@ -6,12 +6,12 @@ from nltk import tokenize
 input_file = 'formatted_one_article_per_line/cord19_one_article_per_line.txt'
 output_file = 'm.input'
 
-#with open(input_file) as fi:
-#    with open(output_file, 'w') as fo:
-#        for line in tqdm.tqdm(fi.readlines()):
-#            sentences = tokenize.sent_tokenize(line)
-#            for sentence in sentences:
-#                fo.write(sentence + '\n')
+with open(input_file) as fi:
+    with open(output_file, 'w') as fo:
+        for line in tqdm.tqdm(fi.readlines()):
+            sentences = tokenize.sent_tokenize(line)
+            for sentence in sentences:
+                fo.write(sentence + '\n')
 
 spm.SentencePieceTrainer.train('--input=m.input --model_prefix=m '
         '--vocab_size=30000 --max_sentence_length=60000 '
