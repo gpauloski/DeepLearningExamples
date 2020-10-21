caca8836bfdd6fe45b8da4dac14c634307816665
Greg Pauloski <jgpauloski@uchicago.edu>
2020-10-21 14:45:56 -0700

    WIP: create_cord19_vocab.py

--- PyTorch/LanguageModeling/BERT/data/create_cord19_vocab.py
+++ PyTorch/LanguageModeling/BERT/data/create_cord19_vocab.py
@@ -1,18 +1,20 @@
 import sentencepiece as spm
-import nltk.data
+import tqdm
+
+from nltk import tokenize
 
 input_file = 'formatted_one_article_per_line/cord19_one_article_per_line.txt'
 output_file = 'm.input'
 
-tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-with open(input_file) as fi:
-    data = fi.read()
-    sentences = tokenizer.tokenize(data)
-    with open(sentence_file, 'w') as fo:
-        fo.write(sentences)
+#with open(input_file) as fi:
+#    with open(output_file, 'w') as fo:
+#        for line in tqdm.tqdm(fi.readlines()):
+#            sentences = tokenize.sent_tokenize(line)
+#            for sentence in sentences:
+#                fo.write(sentence + '\n')
 
 spm.SentencePieceTrainer.train('--input=m.input --model_prefix=m '
-        '--vocab_size=30000 --max_sentence_length=50000'
+        '--vocab_size=30000 --max_sentence_length=60000 '
     )
 
 sp = spm.SentencePieceProcessor()
